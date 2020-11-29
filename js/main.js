daf62f0c1b2b530f628f0f4edde8ca1c3c82e9d8
safaa_ali <safaaaly246@gmail.com>
2020-11-29 03:15:47 +0200

    fix button at register 1 , fix responsive search at tablat

--- js/main.js
+++ js/main.js
@@ -9,9 +9,6 @@ let hide = document.getElementById("hide")
 let password= document.getElementById("password")
 
 
-
-
-
 function changeTo(event) {
   event.preventDefault()
   // console.log('event', event.target.classList)
@@ -33,25 +30,3 @@ function myFunction(e) {
 
 
 
-
-// Function to Hide And Show Password
-
-  show.addEventListener("click",()=>{
-    password.setAttribute("type","text");
-    show.style.display="none"
-  
-    hide.style.display="block";
-  })
-
-hide.addEventListener("click",()=>{
-  password.setAttribute("type","password");
-  show.style.display="block"
-
-  hide.style.display="none";
-})
-
-bulmaCarousel.attach('#slider', {
-  slidesToScroll: 1,
-  slidesToShow: 3,
-  infinite: true,
-});
